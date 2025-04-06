12f0b8db10b543154ed5196077e4bd2d62024f54
root <root@localhost.localdomain>
2025-04-06 23:07:59 +0800

    ddos

---
+++ 12_ddos/build/.ddos.o.d
@@ -0,0 +1,146 @@
+dep_ddos.o = /data/dpdk-stable-19.08.2/dpdk_example_learn/12_ddos/ddos.c \
+ /usr/include/stdc-predef.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_eal.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdint.h \
+ /usr/include/stdint.h /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h /usr/include/bits/wchar.h \
+ /usr/include/sched.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stddef.h \
+ /usr/include/time.h /usr/include/bits/sched.h /usr/include/bits/time.h \
+ /usr/include/bits/timex.h /usr/include/xlocale.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_compat.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_common.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/byteswap-16.h /usr/include/sys/types.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/sys/sysmacros.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/ctype.h \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/stdio_lim.h \
+ /data/dpdk-stable-19.08.2/lib/librte_eal/linux/eal/include/rte_os.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_per_lcore.h \
+ /usr/include/pthread.h /usr/include/bits/setjmp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_bus.h \
+ /usr/include/stdio.h /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdarg.h \
+ /usr/include/bits/sys_errlist.h /usr/include/bits/stdio.h \
+ /usr/include/sys/queue.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_log.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_dev.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_pci_dev_feature_defs.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ethdev.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_interrupts.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_eal_interrupts.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_devargs.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_errno.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ether.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_memcpy.h \
+ /usr/include/string.h /usr/include/bits/string.h \
+ /usr/include/bits/string2.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_vect.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_vect.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mm_malloc.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/immintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xsaveintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/adxintrin.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_random.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_mbuf.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_mempool.h \
+ /usr/include/inttypes.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_spinlock.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_spinlock.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_lcore.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_launch.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_pause.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_pause.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_rtm.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_cpuflags.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_cpuflags.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_branch_prediction.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_common.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_pause.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_cycles.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_cycles.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_debug.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_log.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_atomic.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_atomic.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_atomic_64.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_branch_prediction.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_memory.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_fbarray.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_rwlock.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_rwlock.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_spinlock.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ring.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_memzone.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ring_generic.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_prefetch.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_prefetch.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_byteorder.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/generic/rte_byteorder.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_byteorder_64.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_mbuf_ptype.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_dev_info.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_eth_ctrl.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_flow.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_arp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ether.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_icmp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ip.h \
+ /usr/include/netinet/in.h /usr/include/sys/socket.h \
+ /usr/include/sys/uio.h /usr/include/bits/uio.h \
+ /usr/include/bits/socket.h /usr/include/bits/socket_type.h \
+ /usr/include/bits/sockaddr.h /usr/include/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h /usr/include/bits/in.h \
+ /usr/include/netinet/ip.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_sctp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_tcp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_udp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_esp.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_ethdev_core.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_malloc.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_timer.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_kni.h \
+ /data/dpdk-stable-19.08.2/x86_64-native-linuxapp-gcc/include/rte_pci.h \
+ /data/dpdk-stable-19.08.2/lib/librte_eal/linux/eal/include/rte_kni_common.h \
+ /usr/include/arpa/inet.h /usr/include/math.h \
+ /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+ /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+ /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathinline.h \
+ /data/dpdk-stable-19.08.2/dpdk_example_learn/12_ddos/arp.h
